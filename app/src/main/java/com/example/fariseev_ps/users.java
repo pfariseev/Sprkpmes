17957bb2be5e1941cf2121502a87f55471b72803
Pavel fariseevgit <43645102+pfariseev@users.noreply.github.com>
2025-12-12 16:03:17 +0300

    2025-12-12

--- app/src/main/java/com/example/fariseev_ps/users.java
+++ app/src/main/java/com/example/fariseev_ps/users.java
@@ -476,10 +476,8 @@ public class users extends AppCompatActivity implements AdapterView.OnItemLongCl
                     matrix.preRotate(rotationInDegrees);
                 }
                 bitmap = Bitmap.createBitmap(rotatebitmap, 0, 0, rotatebitmap.getWidth(), rotatebitmap.getHeight(), matrix, true);
-                if (bitmap != null) saveFiletoFolder(Name1, bitmap);
-                else realPath = null;
-                while (!file.canWrite()) {
-                }
+         //       if (bitmap != null) saveFiletoFolder(Name1, bitmap);
+           //     else realPath = null;
                 if (realPath!=null)
                 {
                     try {
@@ -586,14 +584,14 @@ public class users extends AppCompatActivity implements AdapterView.OnItemLongCl
   } */
     private void performCrop(Uri imageUri) {
         // Создаем выходной файл
-      // File outputFile = new File(getCacheDir(), "tempFotoFile" + ".jpg");
+       File outputFile = new File(savephoto.folderToSaveVoid(this,"cache"), Name1 + ".jpg");
 
-        File outputFile = new File(getCacheDir(), "croppedTempFile.jpg") ;
-        if (outputFile.exists())
-        {
-            outputFile.delete();
-            outputFile = new File(getCacheDir(), "croppedTempFile.jpg") ;
-        }
+//        File outputFile = new File(getCacheDir(), "croppedTempFile.jpg") ;
+ //       if (outputFile.exists())
+  //      {
+  //          outputFile.delete();
+   //         outputFile = new File(getCacheDir(), "croppedTempFile.jpg") ;
+    //    }
      //   Uri outputUri;
       //  File cacheDir = getCacheDir();
      //   File outputFile = new File(cacheDir, "cropped_" + System.currentTimeMillis() + ".jpg");
